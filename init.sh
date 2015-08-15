b8cc8f9a7c032b2237ea32e5207bce478513a65a
51test2003 <51test2003@gmail.com>
2015-08-15 23:06:46 +0800

    init script

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+while true
+do
+        process=`ps aux | grep mysqld | grep -v grep`;
+
+        if [ "$process" == "" ]; then
+                sleep 1;
+        else
+                break;
+        fi
+done
+
 INIT=/webdata/ReChat/setup.sh
 
 if [ -f $INIT ]; then
